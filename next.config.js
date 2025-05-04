4bb0a06c29318a14781dcdcc9ef631df083ae6c2
Antony Lozano <lanttom.tdv@gmail.com>
2025-05-04 15:48:34 +0200

    Fix API context typing for Vercel deployment18

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    output: 'standalone', // pour Vercel avec des API Routes
+    experimental: {
+      serverActions: true // facultatif selon ta version de Next.js
+    }
+  };
+  
+  module.exports = nextConfig;
